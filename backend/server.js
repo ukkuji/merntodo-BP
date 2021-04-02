7a55572278f18ccf04dd83a90295d11b95f80eaf
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2021-04-02 14:51:53 +0700

    update Docker Compose

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ let Todo = require('./todo.model');
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+mongoose.connect('mongodb://mongo:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
