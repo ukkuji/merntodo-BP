f93c8902b7ada7f5c2a4274316f93757477cedf7
ukkuji <andi.syukur1990@hotmail.com>
2022-07-14 12:28:05 +0800

    versi 1.0

--- frontend/src/components/create-todo.component.js
+++ frontend/src/components/create-todo.component.js
@@ -51,7 +51,7 @@ export default class CreateTodo extends Component {
             todo_completed: this.state.todo_completed
         };
 
-        axios.post('http://localhost:4000/todos/add', newTodo)
+        axios.post('http://bestagtodo.andisyukur.xyz/todos/add', newTodo)
             .then(res => console.log(res.data));
 
         this.setState({
